7478a22d9041cd08bd72fdec40f1cd3a25972b5f
Jeremy Reynolds <jreynolds323@gmail.com>
2015-01-06 18:29:41 -0600

    added back button

--- autoProject/createNewUser.php
+++ autoProject/createNewUser.php
@@ -11,11 +11,9 @@
 		<meta charset="UTF-8"/>
 		<meta name="viewport" content="width=device-width, user-scalable = no"/>
 		<link rel="icon" href="http://jeremyreynolds.us/favicon.ico"/>
-	</head>
-	<body class="pattern">
 		<?php
 			//connect to sql database
-			$connection = mysqli_connect("localhost", "jeremy", "Snakes12", "auto");
+			$connection = mysqli_connect("localhost", "jeremy", "Tetapuleli^jurtyza", "auto");
 			//errors to be present
 			$firstNameError = $lastNameError = $emailError = $passwordError = $phoneNumberError = $databaseError = $warning = "";
 			//variables for use in validation
@@ -23,7 +21,7 @@
 			$submit = true;
 			if(!$connection){
 				$databaseError = "could not connect to Database";
-				exit();
+				
 			}
 			//if form submit method is post execute new user creation
 			if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -68,10 +66,10 @@
 			//valid entry
 			//email
 			//^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$
-			if(!preg_match("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$", $email){
+			/*if(!preg_match("/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/", $email){
 				$emailError = "email invalid";
 				$submit = false;
-			}
+			}//
 			//password
 			if(!preg_match("/^[a-zA-Z0-9]\w{3,14}$/", $password)){
 				$passwordError = "password invalid";
@@ -82,7 +80,7 @@
 			if(!preg_match("/([0-9][0-9][0-9])-([0-9][0-9][0-9])-([0-9][0-9][0-9][0-9])/", $phoneNumber)){
 				$phoneNumberError = "phone number invalid";
 				$submit = false;
-			}
+			}//*/
 			//if everything passes, submit to customerInfo table
 			if($submit){
 				$query = mysqli_query($connection, "INSERT INTO customerInfo (firstName, lastName, email, phoneNumber, passHash) VALUES ('$firstName', '$lastName', '$email', '$phoneNumber', '$passHash')");
@@ -94,7 +92,9 @@
 			mysqli_close($connection);
 			}
 		?>
-		<!-- Header -->
+	</head>
+	<body class="pattern">
+				<!-- Header -->
 		<header class="header">
 			<div class="container">
 				<div class="profile-content pull-left">
@@ -106,6 +106,7 @@
 					<p><?php echo $passwordError;?></p>
 					<p><?php echo $emailError;?></p>
 					<p><?php echo $phoneNumberError;?></p>
+					<p><?php echo $databaseError;?></p>
 				</div>
 			</div>
 		</header>
@@ -113,7 +114,7 @@
 		<div class="primary col-md-12">
 			<section class="section">
 				<div class="section-inner">
-					<div class="content">
+					<div class="center">
 						<form class="form" method="post" role="form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 							<div class="form-group">
 								<label class="sr-only">First Name</label>
@@ -133,13 +134,19 @@
 							</div>
 							<div class="form-group">
 								<label class="sr-only">Phone Number</label>
-								<input type="password" class="form-control" name="phoneNumber" placeholder="Phone Number ex. 555-555-5555">
+								<input type="text" class="form-control" name="phoneNumber" placeholder="Phone Number ex. 555-555-5555">
 							</div>
+							<button class="back btn btn-default">Go Back</button>
 							<button type="submit" class="btn btn-default">Submit</button>
 						</form>
 					</div>
 				</div>
 			</section>
 		</div>
+		<script>
+			$(".back").on("click", function(){
+				window.location.href = "autoshop.php";
+			});
+		</script>
 	</body>
 </html>
